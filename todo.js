ec811b49f917c858a2e180c9de79743bf218e497
Aqib-dev05 <aqibgithub@gmail.com>
2025-05-25 12:01:00 +0500

    local storage added

--- todo.js
+++ todo.js
@@ -2,19 +2,7 @@ let btn = document.querySelector("#add");
 let inp = document.querySelector("input");
 let ul = document.querySelector("ul");
 
-btn.addEventListener("click", function () {
-    if(inp.value !=""){
-  let list = document.createElement("li"); //new list
-  list.innerText = inp.value; //list text
 
-  let del = document.createElement("button"); //delete button
-  del.classList.add("del");
-  del.innerText = "delete";
-  list.appendChild(del); //delete nutton append to list
-
-  ul.append(list); //list append to ul
-  inp.value = "";}
-});
 
 // Save tasks to localStorage when adding new task
 btn.addEventListener("click", function () {
